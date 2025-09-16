c07ea763f75be798231c4d1e7463a305459629f3
Raven Carlos T. Rodrigo <ravencarlostrodrigo@gmail.com>
2025-09-16 14:42:25 +0800

    feat(api): scaffold spring boot project
    Scaffold spring boot project with Java. Bootstrapp by Spring Initialzr
    web-based tool to have minimal spring configuration. The project uses
    OpenJDK 17 and Spring Boot version 3.5.5.

---
+++ loan-calculator-api/src/main/java/com/rctrodrigo/loan_calculator_api/LoanCalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.rctrodrigo.loan_calculator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalculatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCalculatorWebApplication.class, args);
+	}
+
+}
---
+++ loan-calculator-api/src/test/java/com/rctrodrigo/loan_calculator_api/LoanCalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rctrodrigo.loan_calculator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
